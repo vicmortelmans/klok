a0cb2837df11f928342cdd5274383e16bc57ead8
Vic Mortelmans <vicmortelmans@gmail.com>
2024-09-11 21:17:00 +0200

    refactoring

---
+++ bells.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import klok_lib
+
+klok_lib.init()
+klok_lib.turn(9, dir=True, motor="bells")
+
--- klok_adjust_bells.py
+++
@@ -1,6 +0,0 @@
-import klok_lib
-
-
-klok_lib.init()
-klok_lib.turn(0.2, brake=2, step=klok_lib.bells_step)
-
