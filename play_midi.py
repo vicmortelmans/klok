9622b8693c8557cc216a8653eec362985ea09f26
Vic Mortelmans <vicmortelmans@gmail.com>
2024-10-02 16:46:08 +0200

    wip

--- play_midi.py
+++ play_midi.py
@@ -2,8 +2,13 @@
 import klok_lib
 import asyncio
 import mido
+
+# initialize the GPIO
+klok_lib.init()
+
 mid = mido.MidiFile('flash1.mid')
 for msg in mid.play():
     if msg.type == 'note_on':
+        print(msg.note)
         klok_lib.play_solenoid(msg.note)
 
