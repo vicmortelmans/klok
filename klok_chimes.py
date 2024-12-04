99b9ef9de1b0b76199328d7819be0072ab50b962
Vic Mortelmans <vicmortelmans@gmail.com>
2024-12-04 13:03:38 +0100

    fix chimes scripts

--- klok_chimes.py
+++ klok_chimes.py
@@ -1,10 +1,13 @@
 import klok_lib
 import RPi.GPIO as GPIO
+import time
 
 klok_lib.init()
 
-# 1/16th of a sound
-klok_lib.turn(0.00625, brake=2, step=klok_lib.chime_step)
+while True:
+    # 1/16th of a sound
+    klok_lib.turn(0.313, motor="chimes")
+    time.sleep(1.0)
 
 GPIO.output(klok_lib.sleep, False)
 
