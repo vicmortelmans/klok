86eca5317b3ca5009c6c74a9ac0a871bbf9db2e7
Vic Mortelmans <vicmortelmans@gmail.com>
2024-12-08 17:00:52 +0100

    fixes

--- bells.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import klok_lib
-
-klok_lib.init()
-klok_lib.turn(1/3, dir=True, motor="bells")
-
--- klok_test_bells.py
+++ klok_test_bells.py
@@ -4,11 +4,10 @@ import RPi.GPIO as GPIO
 klok_lib.init()
 
 try:
-	while True:
-		klok_lib.turn(1, brake=2, step=klok_lib.bells_step)
+    klok_lib.turn(1/3, dir=True, motor="bells")
 
 except KeyboardInterrupt:
-	GPIO.output(klok_lib.sleep, False)
-	print 'Interrupted!'
+    GPIO.output(klok_lib.sleep, False)
+    print('Interrupted!')
         
 
